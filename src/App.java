9ba91fe66580d5aa27c1885581e8a5c7ed64a747
Rasmussen <rasmussenj@bsd405.org>
2020-06-08 08:44:51 -0700

    chore: initial commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+// !! NOTE: This is a placeholder class !!
+// You may delete the contents of this file and put your project here :) 
+// 
+// You are encouraged to break your code into multiple files and
+// not write all the code in this single file. 
+//
+// Remember this is our "Final" project but the intent of this project
+// is to have fun and illustrate what you have learned this year. 
+// I'm just looking for you to try something and I'm not looking for 
+// perfection. Be creative and have fun with it :)  
+
+public class App {
+
+}
